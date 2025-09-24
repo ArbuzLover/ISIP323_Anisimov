bc90a75e7e46c2da4bf76b4d787c7dddef38b8fd
ArbuzLover <Yan201901@yandex.ru>
2025-09-24 16:09:03 +0300

    Дописал все функции для меню и они точно работают!

--- ISIP323_Anisimov/Program.cs
+++ ISIP323_Anisimov/Program.cs
@@ -14,6 +14,7 @@ while(Text.Length < 100)
 
 }
 string[] TextList = Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
 while (true)
 {
     Console.WriteLine("\n=== МЕНЮ ===");
@@ -22,7 +23,7 @@ while (true)
     Console.WriteLine("3 - Количество предложений в тексте");
     Console.WriteLine("4 - Количество гласных и согласных букв в тексте");
     Console.WriteLine("5 - Самое длинное слово");
-    //Console.WriteLine("6 - Поиск товаров (по коду, названию, категории)");
+    Console.WriteLine("6 - Статистика каждой буквы");
     //Console.WriteLine("7 - История продаж (и отмена последней продажи)");
     //Console.WriteLine("8 - Отчёт о продажах");
     Console.WriteLine("0 - Выход");
@@ -34,9 +35,9 @@ while (true)
         case "1": KolvoWords(); break;
         case "2": ShortWord(); break;
         case "3": KolvoPredl(); break;
-        case "4": ; break;
+        case "4": KolvoGlasnSoglas(); break;
         case "5": LongWord(); break;
-        case "6": ; break;
+        case "6": Stat(); break;
         case "0": return;
         default: Console.WriteLine("Неверная команда. Попробуйте снова."); break;
     }
@@ -69,12 +70,82 @@ void LongWord()
 
 void KolvoPredl()
 {
+    char[] ABC = { 'А', 'О', 'У', 'Э', 'И', 'Ы', 'Е', 'Ё', 'Ю', 'Я', 'Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ъ' };
+    char[] prep = {'.','!','?',};
     int count = 0;
-    foreach (int i in Text)
+    for (int i = 0; i < Text.Length; i++)
     {
-        if (Text[i] == '.') count++;
+        if (prep.Contains(Text[i]) && ABC.Contains(Text[i - 1])) { continue; }
+        else if(prep.Contains(Text[i])) { count++; } 
     }
     Console.WriteLine($"Количество предложений в тексте: {count}");
 }
 
+void KolvoGlasnSoglas()
+{
+    char[] GlasList = { 'А', 'О', 'У', 'Э', 'И', 'Ы', 'Е', 'Ё', 'Ю', 'Я', 'а', 'о', 'у', 'з', 'и', 'ы', 'е', 'ё', 'ю', 'я' };
+    char[] SoglasList = { 'Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ъ',
+    'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ъ' };
+    int countS = 0, countG = 0;
+    foreach (char i in Text)
+    {
+        
+        if (SoglasList.Contains(i)) { countS++; }
+        else if (GlasList.Contains(i)) { countG++; }
+    }
+    Console.WriteLine($"Количество гласных: {countG}, количество согласных: {countS} ");
+}
 
+void Stat()
+{
+    Dictionary<char, int> stat = new Dictionary<char, int>() {
+        {'А', 0 },
+        {'Б',0},
+        {'В',0},
+        {'Г',0},
+        {'Д',0},
+        {'Е',0},
+        {'Ё',0},
+        {'Ж',0},
+        {'З',0},
+        {'И',0},
+        {'Й',0},
+        {'К',0},
+        {'Л',0},
+        {'М',0},
+        {'Н',0},
+        {'О',0},
+        {'П',0},
+        {'Р',0},
+        {'С',0},
+        {'Т',0},
+        {'У',0},
+        {'Ф',0},
+        {'Х',0},
+        {'Ц',0},
+        {'Ч',0},
+        {'Ш',0},
+        {'Щ',0},
+        {'Ъ',0},
+        {'Ы',0},
+        {'Ь',0},
+        {'Э',0},
+        {'Ю',0},
+        {'Я',0},
+
+
+    };
+    foreach (char c in Text.ToUpper()) {
+    if (stat.ContainsKey(c))
+        {
+            stat[c]++;
+        }
+    }
+    foreach (var pair in stat.OrderByDescending(x => x.Value)) {
+        if (pair.Value > 0)
+        {
+            Console.WriteLine($"Буква '{pair.Key}' встречается {pair.Value} раз");
+        }
+    }
+
+}
