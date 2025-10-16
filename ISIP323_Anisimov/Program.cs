bd050bc4444556b7d385b2053759efdcd28e2381
234918 <234918@KB37-306-C07>
2025-10-16 11:54:34 +0300

    Декомпозиция: добавил класс Teacher на основе Person и так же добавил класс Course

--- ISIP323_Anisimov/Program.cs
+++ ISIP323_Anisimov/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+
 public enum Sex
 {
     men = 0,
@@ -10,29 +11,30 @@ class Person
 {
     private string FIO;
     private Sex Sex;
+    private static int ids = 0;
+    private int ID;
 
     public Person(string fio, Sex sex)
     {
+        ids++;
+        ID = ids;
         FIO = fio;
        Sex =sex;
     }
     public virtual void Print()
     {
-        Console.WriteLine($"ФИО: {FIO}\n Пол: {Sex} ");
+        Console.WriteLine($"Номер: {ID}\n ФИО: {FIO}\n Пол: {Sex} ");
     }
 }
 
 class Student : Person
 {
-    private static int ids = 0;
-    private int StudentID;
-    private int CourseNumber;
+
+    private int CourseNumber { get; set; }
 
     public Student(string fio,  Sex sex,  int CourseNumber)
         : base(fio, sex)
     {
-        ids++;
-        StudentID = ids;
         this.CourseNumber = CourseNumber;
     }
 
@@ -40,6 +42,35 @@ class Student : Person
     {
         Console.WriteLine("Student");
         base.Print();
-        Console.WriteLine($"Номер студента: {StudentID}\n Курс обучения: {CourseNumber}\n");
+        Console.WriteLine($" Курс обучения: {CourseNumber}\n");
+    }
+}
+
+class Teacher : Person
+{
+    private int Exp { get; set; }
+    public Teacher(string fio, Sex sex, int exp) : base(fio, sex) {
+        this.Exp = exp;
+    }
+    public override void Print()
+    {
+        Console.WriteLine("Teacher");
+        base.Print();
+        Console.WriteLine($"Опыт работы: {Exp}\n");
+    }
+}
+
+class Course
+{
+    private string NameCourse { get; set; }
+    private string Time { get; set; }
+    private Teacher Teacher { get; set; }
+
+    public Course(string name_cour, string time, Teacher teacher)
+    {
+
+        this.NameCourse = name_cour;
+        this.Time = time;
+        this.Teacher = teacher;
     }
 }
