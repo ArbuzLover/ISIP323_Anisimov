fe8277650320c4fd673c11eace4aca0add1ac4f3
ArbuzLover <Yan201901@yandex.ru>
2025-10-20 23:15:43 +0300

    Декомпозиция: Написал класс Game, который по факту будет выполнять все действия и написал пару функций внутри него, которые было легче всего реализовать.

--- ISIP323_Anisimov/Program.cs
+++ ISIP323_Anisimov/Program.cs
@@ -165,3 +165,193 @@ public class Boss : Enemy
    
     }
 }
+
+
+//-------------------------------------------ИГРА-------------------------------------------
+
+public class Game
+{
+    private Player player;
+    private Random random;
+    private int Count;
+    private List<Weapon> weapons;
+    private List<Armor> armors;
+
+    public Game()
+    {
+        player = new Player();
+        random = new Random();
+        Count = 0;
+        
+    }
+
+    
+
+    public void StartGame()
+    {
+        Console.WriteLine("Добро пожаловать в игру!");
+        Console.WriteLine("Сражайтесь с врагами, находите сокровища и выживайте!\n");
+
+        while (player.Health > 0)
+        {
+            Play();
+        }
+
+        GameOver();
+    }
+
+    private void Play()
+    {
+        Count++;
+        PrintPlayerStatus();
+
+        if (player.IsFrozen)
+        {
+            FrozenCheck();
+            return;
+        }
+
+        ShowMenu();
+    }
+
+    private void PrintPlayerStatus()
+    {
+
+    }
+
+    private void FrozenCheck()
+    {
+        Console.WriteLine("❄️ Вы заморожены и пропускаете ход!");
+        player.IsFrozen = false;
+    }
+
+    private void ShowMenu()
+    {
+        Console.WriteLine("\n1. Продолжить путешествие");
+        Console.WriteLine("2. Выйти из игры");
+
+        var input = Console.ReadLine();
+        switch (input)
+        {
+            case "1":
+                ProcessTurn();
+                break;
+            case "2":
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Неверный ввод!");
+                ShowMenu();
+                break;
+        }
+    }
+
+    private void ProcessTurn()
+    {
+        if (Count % 10 == 0) // каждыу 10 ходов босс
+        {
+            FindBoss();
+        }
+        else
+        {
+            if (random.NextDouble() < 0.5) // 50% шанс на врага или сундук
+            {
+                FindEnemy();
+            }
+            else
+            {
+                FindChest();
+            }
+        }
+    }
+
+    private void FindEnemy()
+    {
+
+    }
+
+    private Enemy CreateEnemy(EnemyType type)
+    {
+        switch (type)
+        {
+            case EnemyType.Goblin: return new Enemy(EnemyType.Goblin, 30, 8, 5);
+            case EnemyType.Skeleton: return new Enemy(EnemyType.Skeleton, 25, 10, 3);
+            case EnemyType.Mage: return new Enemy(EnemyType.Mage, 20, 12, 1);
+            default: return new Enemy(EnemyType.Goblin,30, 8, 5);
+        }
+    }
+
+    private void PrintEnemyInfo(Enemy enemy)
+    {
+        Console.WriteLine($"❤ Здоровье врага: {enemy.Health} |  Атака: {enemy.Attack} |  Защита: {enemy.Defense}");
+    }
+
+    private void FindBoss()
+    {
+
+    }
+
+    private Boss CreateBoss(BossType type)
+    {
+        switch(type)
+        {
+            case BossType.VVG: return new Boss(BossType.VVG, 40, 10, 3);
+            case BossType.Kovalevsky: return new Boss(BossType.Kovalevsky, 35, 12, 4);
+            case BossType.ArchmageCPP: return new Boss(BossType.ArchmageCPP, 25, 15, 2);
+            case BossType.PestovCMinus: return new Boss(BossType.PestovCMinus, 20, 18, 1);
+            default: return new Boss(BossType.VVG, 40, 10, 3);
+        }
+    }
+
+    private void Combat(Enemy enemy)
+    {
+
+    }
+
+    private void ShowCombatMenu()
+    {
+        Console.WriteLine("\n1. Атаковать");
+        Console.WriteLine("2. Защищаться");
+    }
+
+    private void FindChest()
+    {
+        Console.WriteLine("\n Вы нашли сундук!");
+
+        var chestType = random.Next(0, 3);
+
+        switch (chestType)
+        {
+            case 0:
+                GiveHealing();
+                break;
+            case 1:
+                GiveWeapon();
+                break;
+            case 2:
+                GiveArmor();
+                break;
+        }
+    }
+
+    private void GiveHealing()
+    {
+        Console.WriteLine(" В сундуке лечебное зелье!");
+        player.Heal();
+    }
+
+    private void GiveWeapon()
+    {
+
+    }
+
+    private void GiveArmor()
+    {
+
+    }
+
+    private void GameOver()
+    {
+
+    }
+}
