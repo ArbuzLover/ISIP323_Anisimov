41cda5b6f73edafc6956386173c637142c8b59fe
ArbuzLover <Yan201901@yandex.ru>
2025-10-20 20:04:34 +0300

    Декомпозиция: Добавил класс для боссов на основе класса врагов, самое сложное было написать определение расы босса по типу босса, короче тяжело!!!!

--- ISIP323_Anisimov/Program.cs
+++ ISIP323_Anisimov/Program.cs
@@ -1,4 +1,5 @@
-﻿public class Weapon
+﻿// -----------------------------------ИГРОКИ-------------------------------------------
+public class Weapon
 {
     public string Name { get; set; }
     public int Damage { get; set; }
@@ -60,7 +61,7 @@ public class Player
     }
 
 }
-
+//---------------------------------ВРАГИ------------------------
 public enum EnemyType
 {
     Goblin,
@@ -90,12 +91,77 @@ public class Enemy
 
     }
 
-    public void AttackPlayer(Player player)
+    public virtual void AttackPlayer(Player player)
     {
        
     }
 }
+//----------------------------БОССЫ--------------------------
+public enum BossType
+{
+    VVG,
+    Kovalevsky,
+    ArchmageCPP,
+    PestovCMinus
+}
+public class Boss : Enemy
+{
+    public BossType BossType { get; set; }
 
+    public Boss(BossType type, int baseHealth, int baseAttack, int baseDefense)
+        : base(GetEnemyTypeFromBoss(type), 0, 0, 0)
+    {
+        BossType = type;
 
+        switch (type)
+        {
+            case BossType.VVG:
+                Health = (int)(baseHealth * 2.0);
+                Attack = (int)(baseAttack * 1.5);
+                Defense = (int)(baseDefense * 1.2);
+                break;
 
+            case BossType.Kovalevsky:
+                Health = (int)(baseHealth * 2.5);
+                Attack = (int)(baseAttack * 1.3);
+                Defense = (int)(baseDefense * 1.4);
+                break;
+
+            case BossType.ArchmageCPP:
+                Health = (int)(baseHealth * 1.8);
+                Attack = (int)(baseAttack * 1.6);
+                Defense = (int)(baseDefense * 1.1);
+                break;
+
+            case BossType.PestovCMinus:
+                Health = (int)(baseHealth * 1.3);
+                Attack = (int)(baseAttack * 1.8);
+                Defense = (int)(baseDefense * 0.6);
+                break;
+        }
+
+        MaxHealth = Health;
+    }
+
+    private static EnemyType GetEnemyTypeFromBoss(BossType bossType)
+    {
+        switch (bossType)
+        {
+            case BossType.VVG:
+                return EnemyType.Goblin;
+            case BossType.Kovalevsky:
+                return EnemyType.Skeleton;
+            case BossType.ArchmageCPP:
+                return EnemyType.Mage;
+            case BossType.PestovCMinus:
+                return EnemyType.Skeleton;
+            default:
+                return EnemyType.Goblin;
+        }
+    }
+
+    public override void AttackPlayer(Player player)
+    {
+   
+    }
 }
